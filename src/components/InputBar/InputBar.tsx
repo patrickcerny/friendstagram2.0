2aeb182daeb6032b26914e3294b0a413a0e2aa88
Patrick Cerny <patrick.cerny04@gmail.com>
2022-01-02 14:05:11 +0100

    added keydown (enter) events

--- src/components/InputBar/InputBar.tsx
+++ src/components/InputBar/InputBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import User from '../../utils/models/user.model';
 import './InputBar.scss';
 interface InputBarProps {
@@ -9,19 +9,28 @@ interface InputBarProps {
 const InputBar = (props: InputBarProps) => {
   const [message, setMessage] = useState('');
 
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter' && !e.shiftKey) return handleOnSend();
+  };
+
   const handleOnSend = () => {
-    props.onSend(message);
+    if (message.trim().length <= 0) return;
+
+    props.onSend(message.trim());
     setMessage('');
   };
   return (
     <div className="main-inputbar">
       <img src={props.user.profile_picture} alt={props.user.username} />
       <textarea
-        maxLength={200}
+        maxLength={1000}
         value={message}
         onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={(e) => handleKeyDown(e)}
       />
-      <button onClick={handleOnSend}>Senden</button>
+      <button onClick={handleOnSend} className="main-button">
+        Senden
+      </button>
     </div>
   );
 };
